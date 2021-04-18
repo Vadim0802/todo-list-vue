a169bc6eab5c437458d23affffd265aacaa8eed6
Vadim Bodianskii <v.bodyansky@yandex.ru>
2021-04-18 23:34:00 +1000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="control-panel">
+    <form v-on:submit.prevent="addTask">
+      <div class="mb-3">
+        <h2 class="display-4">Заметка</h2>
+        <input type="text" class="form-control" name="task-name" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Добавить</button>
+    </form>
+  </div>
+  <h3 class="display-6">Число заметок: {{tasks.length}}</h3>
+  <br>
+  <div class="container-xxl">
+    <ul class="list-group" v-if="tasks.length !== 0">
+      <li
+          class="list-group-item"
+          v-for="(task, index) in tasks"
+          v-bind:key="task"
+      >
+        <samp>{{ task }}</samp>
+        <button class="btn btn-danger" v-on:click="removeTask(index)">Удалить</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(event) {
+      const formData = new FormData(event.target);
+      const nameTask = formData.get('task-name');
+
+      if (!this.tasks.includes(nameTask)) {
+        this.tasks.push(nameTask);
+      }
+    },
+    removeTask(index) {
+      this.tasks = this.tasks.filter((_, idx) => index !== idx);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .list-group-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .control-panel {
+    width: 400px;
+  }
 </style>
